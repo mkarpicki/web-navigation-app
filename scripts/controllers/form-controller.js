e01e3ad189f1c73521b94273ff6b2aa957112611
Mariusz Karpicki <mkarpicki@gmail.com>
2015-11-15 16:22:34 +0100

    [prototype] : replacing state after search click to have pre filled form when coming back (maybe temporal solution)

--- scripts/controllers/form-controller.js
+++ scripts/controllers/form-controller.js
@@ -1,19 +1,45 @@
 angular.module('navigationApp.controllers').controller('FormController',
-    ["$scope", '$interpolate', '$location', function($scope, $interpolate, $location) {
+    ["$scope", '$interpolate', '$location', '$timeout', function($scope, $interpolate, $location, $timeout) {
 
         'use strict';
 
-        //$scope.from = '52.40626,13.49667';
-        //$scope.to = '52.51083,13.45264';
+        $scope.from = '52.40626,13.49667';
+        $scope.to = '52.51083,13.45264';
 
-        //$scope.wayPoints = ['52.46325,13.3882'];
-        $scope.wayPoints = [];
+        $scope.wayPoints = ['52.46325,13.3882'];
 
         $scope.getRoute = function () {
 
             if (!$scope.from || !$scope.to) {
                 return;
             }
+
+            var query = buildSearchQuery();
+
+            $location.url('/?' + query).replace();
+
+            /**
+             * @readme
+             * used timeout here to replace state with filled form
+             * and then go to search
+             * will consider different approach like keeping waypoints in session storage and
+             * use it if no deep link to pre fill form, lets see
+             */
+            $timeout(function () {
+                $location.url('/search?' + query );
+            }, 500);
+
+        };
+
+        $scope.addWayPoint = function () {
+            $scope.wayPoints.push('');
+        };
+
+        $scope.removeWayPoint = function (index) {
+            $scope.wayPoints.splice(index, 1);
+        };
+
+        var buildSearchQuery = function () {
 
             var allPoints = [$scope.from].concat($scope.wayPoints).concat($scope.to),
                 waypointQuery = "{{separator}}waypoint{{i}}={{waypoint}}",
@@ -33,33 +59,8 @@ angular.module('navigationApp.controllers').controller('FormController',
                 separator = '&';
             }
 
-            //$location.url('/?' + query );
-            $location.url('/search?' + query );
-
+            return query;
         };
-
-        $scope.addWayPoint = function () {
-            $scope.wayPoints.push('');
-        };
-
-        $scope.removeWayPoint = function (index) {
-            $scope.wayPoints.splice(index, 1);
-        };
-
-        //$scope.$on('$routeChangeSuccess',function(evt, absNewUrl, absOldUrl) {
-        //
-        //    console.log($scope.from , $scope.to);
-        //
-        //    if ($scope.from && $scope.to) {
-        //
-        //        console.log('success', evt, absNewUrl, absOldUrl);
-        //
-        //        if ( absNewUrl.$$route.originalPath === (absOldUrl && absOldUrl.$$route.originalPath) ) {
-        //            console.log('go to search');
-        //            $location.url('/search?' + $location.$$url);
-        //        }
-        //    }
-        //});
 
         var getReady = function(){
 
