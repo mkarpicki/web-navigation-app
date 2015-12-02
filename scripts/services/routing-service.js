140d2a7fe2e233caf9eec56b76134594c53374c1
Mariusz Karpicki <mariusz.karpicki@here.com>
2015-12-02 02:12:39 +0100

    [prototype] : added  support for multi areas to avoid

--- scripts/services/routing-service.js
+++ scripts/services/routing-service.js
@@ -39,7 +39,7 @@ angular.module('navigationApp.services').factory('routingService', ['$http', '$q
 
     var buildAvoidAreasQuery = function (areasToAvoid) {
 
-        //areasToAvoid = [areasToAvoid.join(';')];
+        areasToAvoid = [areasToAvoid.join('!')];
 
         /**
          * @fixme - this format will always take last added are to avoid
