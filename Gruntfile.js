947da23382306331f130c5ffaf049e5750207d8a
Mariusz Karpicki <mariusz.karpicki@here.com>
2015-12-27 03:39:06 +0100

    generating of tests coverage after each run

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,6 +59,30 @@ module.exports = function(grunt) {
                 // CLI --browsers Chrome,Firefox,Safari
                 browsers: ['PhantomJS'],
 
+                // preprocess matching files before serving them to the browser
+                // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+                preprocessors: {
+                    './scripts/**/*.js': ['coverage']
+                },
+
+                // test results reporter to use
+                // possible values: 'dots', 'progress'
+                // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+                reporters: ['progress', 'coverage', 'html'],
+
+                htmlReporter: {
+                    outputDir: './test-results/karma', // where to put the reports
+                    templatePath: null, // set if you moved jasmine_template.html
+                    focusOnFailures: true, // reports show failures on start
+                    namedFiles: false, // name files instead of creating sub-directories
+                    pageTitle: null, // page title for reports; browser info by default
+                    urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
+
+                    // experimental
+                    preserveDescribeNesting: false, // folded suites stay folded
+                    foldAll: false // reports start folded (only with preserveDescribeNesting)
+                },
+
                 coverageReporter: {
                     type: 'html',
                     dir: 'test/results/coverage/'
