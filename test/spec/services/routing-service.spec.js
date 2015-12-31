dc0da752941aca012ec8dc7f3a538770b5593428
Mariusz Karpicki <mariusz.karpicki@here.com>
2015-12-31 18:50:37 +0100

    unit tests: routingService (working on 'calculateWithTrafficEnabled')

--- test/spec/services/routing-service.spec.js
+++ test/spec/services/routing-service.spec.js
@@ -6,24 +6,36 @@ describe('routingService', function () {
     var config,
         $http,
         $q,
-        $interpolate;
+        $interpolate,
+
+        //$httpBackend,
+
+        fakeExp;
 
 
     beforeEach(module('navigationApp.services'));
 
-
     beforeEach(module(function ($provide) {
 
-
-        //$interpolate = {};
+        //fakeExp = function () {};
+        //
+        //$interpolate = function () {
+        //    return fakeExp;
+        //};
+        //
         //$provide.value('$interpolate', $interpolate);
         //
-        //$q = {};
+        //$q = {
+        //    then: function () {},
+        //    defer: function () {}
+        //};
+        //
         //$provide.value('$q', $q);
         //
-        //$http = _$http_;
-        //$http.value('$http', $http);
-
+        //$http = {
+        //    get: function () { return $q; }
+        //};
+        //$provide.value('$http', $http);
 
         config = {};
         config.appId = 'someAppId';
@@ -71,6 +83,70 @@ describe('routingService', function () {
             expect(routingService.getResults().length).toEqual(0);
 
         }))
+
+    });
+
+    describe('calculateWithTrafficEnabled', function () {
+
+        var waypoints,
+            areasToAvoid,
+            URL;
+
+        beforeEach(function (){
+
+            waypoints = [
+                '52.5182,13.4279',
+                '52.5138,13.4733'
+            ];
+
+            areasToAvoid = [
+                '52.516,13.453;52.514,13.455',
+                '52.517,13.454;52.515,13.456'
+            ];
+
+            URL = "https://route.api.here.com/routing/7.2/calculateroute.json?" +
+                "app_id={{appId}}" +
+                "&app_code={{appCode}}" +
+                "{{wayPoints}}" +
+                "{{avoidAreas}}" +
+                "&alternatives={{alternatives}}" +
+                "&legattributes=sm" +
+                "&linkattributes=" +
+                "&maneuverattributes=all" +
+                "&metricSystem=metric" +
+                "&mode=fastest;car;traffic:{{traffic}};" +
+                "&routeattributes=none,sh,wp,sm,bb,lg,no,li,tx" +
+                "&transportModeType=car";
+
+        });
+
+
+        it ('should get route with option of enabled traffic', inject(function (routingService, $httpBackend) {
+
+            //$http.get = jasmine.createSpy('$http.get').and.returnValue($q);
+
+            //$httpBackend
+            //    .when('GET', 'https://route.api.here.com/routing/7.2/calculateroute.json').respond(200, {
+            //        status: "ERROR",
+            //        data: []
+            //    });
+
+            //$httpBackend.expectGET('https://route.api.here.com/routing/7.2/calculateroute.json').respond(200, {
+            $httpBackend.expectGET(URL).respond(200, {
+                data: {
+                    response: {
+                        route: [1,2,3]
+                    }
+                }
+            });
+
+
+            var res = routingService.calculateWithTrafficEnabled(waypoints, areasToAvoid);
+
+            console.log(res);
+            //expect($http.get).toHaveBeenCalledWith();
+
+        }));
 
     });
 
