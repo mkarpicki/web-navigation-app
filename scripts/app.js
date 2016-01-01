bedda18abbd9f199a64e503d359aa29a283f96e0
Mariusz Karpicki <mariusz.karpicki@here.com>
2016-01-01 04:42:50 +0100

    removed empty config definition in app.'s

--- scripts/app.js
+++ scripts/app.js
@@ -8,4 +8,5 @@ angular.module('navigationApp', [
     'navigationApp.controllers',
     'navigationApp.services',
     'navigationApp.directives'
-]).config( function () {});
+]);
+//]).config( function () {});
