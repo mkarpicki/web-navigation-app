bd564e1316c96f6c6e862d8fdc3477e496420cbd
Mariusz Karpicki <mariusz.karpicki@here.com>
2016-01-10 20:28:27 +0100

    e2e test. form page. clearing of form (clearing waypoints)

--- test/e2e/test.spec.js
+++ test/e2e/test.spec.js
@@ -42,92 +42,128 @@ describe('Navigation App', function () {
             browser.get(getHost());
         });
 
-        it('should redirect to search results page after entering start and destination positions', function() {
+        describe('get route button', function () {
 
-            element.all(by.css(SELECTORS.INPUT_FROM)).sendKeys(fromPosition);
-            element.all(by.css(SELECTORS.INPUT_TO)).sendKeys(toPosition);
-            element.all(by.css(SELECTORS.BTN_GET_ROUTE)).first().click();
-
-            browser.getCurrentUrl().then(function(url) {
-                expect(url).toEqual(getHost() + "/search?w0=" + fromPosition + "&w1=" + toPosition);
-            });
-
-        });
-
-        it('should provide clear functionality that rests form', function () {
-
-            element.all(by.css(SELECTORS.INPUT_FROM)).sendKeys(fromPosition);
-            element.all(by.css(SELECTORS.INPUT_TO)).sendKeys(toPosition);
-
-            expect(element(by.css(SELECTORS.INPUT_FROM)).getAttribute('value')).toEqual(fromPosition);
-            expect(element(by.css(SELECTORS.INPUT_TO)).getAttribute('value')).toEqual(toPosition);
-
-            element.all(by.css(SELECTORS.BTN_CLEAR)).first().click();
-
-            expect(element(by.css(SELECTORS.INPUT_FROM)).getAttribute('value')).toEqual('');
-            expect(element(by.css(SELECTORS.INPUT_TO)).getAttribute('value')).toEqual('');
-
-        });
-
-        describe('and middle way points are added', function () {
-
-            it('should redirect to search results page with middle way points', function () {
-
-                var expectedUrl = getHost() + "/search?w0=" + fromPosition + "&w1=" + wayPoints[0] + "&w2=" + wayPoints[1] + "&w3=" + toPosition;
+            it('should redirect to search results page after entering start and destination positions', function() {
 
                 element.all(by.css(SELECTORS.INPUT_FROM)).sendKeys(fromPosition);
-
-                element.all(by.css(SELECTORS.BTN_ADD_WAY_POINT)).first().click();
-                element.all(by.css(SELECTORS.INPUT_WAY_POINT + 0)).sendKeys(wayPoints[0]);
-
-                element.all(by.css(SELECTORS.BTN_ADD_WAY_POINT)).first().click();
-                element.all(by.css(SELECTORS.INPUT_WAY_POINT + 1)).sendKeys(wayPoints[1]);
-
                 element.all(by.css(SELECTORS.INPUT_TO)).sendKeys(toPosition);
                 element.all(by.css(SELECTORS.BTN_GET_ROUTE)).first().click();
 
                 browser.getCurrentUrl().then(function(url) {
-
-                    expect(url).toEqual(expectedUrl);
+                    expect(url).toEqual(getHost() + "/search?w0=" + fromPosition + "&w1=" + toPosition);
                 });
 
             });
 
-            it('should allow to remove add way points', function () {
+            describe('and middle way points are added', function () {
 
-                var expectedUrl;
+                it('should redirect to search results page with middle way points', function () {
 
-                element.all(by.css(SELECTORS.INPUT_FROM)).sendKeys(fromPosition);
+                    var expectedUrl = getHost() + "/search?w0=" + fromPosition + "&w1=" + wayPoints[0] + "&w2=" + wayPoints[1] + "&w3=" + toPosition;
 
-                element.all(by.css(SELECTORS.BTN_ADD_WAY_POINT)).first().click();
-                element.all(by.css(SELECTORS.INPUT_WAY_POINT + 0)).sendKeys(wayPoints[0]);
+                    element.all(by.css(SELECTORS.INPUT_FROM)).sendKeys(fromPosition);
 
-                element.all(by.css(SELECTORS.BTN_ADD_WAY_POINT)).first().click();
-                element.all(by.css(SELECTORS.INPUT_WAY_POINT + 1)).sendKeys(wayPoints[1]);
+                    element.all(by.css(SELECTORS.BTN_ADD_WAY_POINT)).first().click();
+                    element.all(by.css(SELECTORS.INPUT_WAY_POINT + 0)).sendKeys(wayPoints[0]);
 
-                element.all(by.css(SELECTORS.INPUT_TO)).sendKeys(toPosition);
+                    element.all(by.css(SELECTORS.BTN_ADD_WAY_POINT)).first().click();
+                    element.all(by.css(SELECTORS.INPUT_WAY_POINT + 1)).sendKeys(wayPoints[1]);
 
-                element.all(by.css(SELECTORS.BTN_REMOVE_WAY_POINT)).last().click();
+                    element.all(by.css(SELECTORS.INPUT_TO)).sendKeys(toPosition);
+                    element.all(by.css(SELECTORS.BTN_GET_ROUTE)).first().click();
 
-                browser.getCurrentUrl().then(function(url) {
+                    browser.getCurrentUrl().then(function(url) {
 
-                    expectedUrl = getHost() + "/?w0=" + fromPosition + "&w1=" + wayPoints[0] + "&w2=" + toPosition;
+                        expect(url).toEqual(expectedUrl);
+                    });
 
-                    expect(url).toEqual(expectedUrl);
-                });
-
-                element.all(by.css(SELECTORS.BTN_REMOVE_WAY_POINT)).last().click();
-
-                browser.getCurrentUrl().then(function(url) {
-
-                    expectedUrl = getHost() + "/?w0=" + fromPosition + "&w1=" + toPosition;
-
-                    expect(url).toEqual(expectedUrl);
                 });
 
             });
 
         });
+
+        describe('clear button', function () {
+
+            it('should provide clear functionality that rests form', function () {
+
+                element.all(by.css(SELECTORS.INPUT_FROM)).sendKeys(fromPosition);
+                element.all(by.css(SELECTORS.INPUT_TO)).sendKeys(toPosition);
+
+                expect(element(by.css(SELECTORS.INPUT_FROM)).getAttribute('value')).toEqual(fromPosition);
+                expect(element(by.css(SELECTORS.INPUT_TO)).getAttribute('value')).toEqual(toPosition);
+
+                element.all(by.css(SELECTORS.BTN_CLEAR)).first().click();
+
+                expect(element(by.css(SELECTORS.INPUT_FROM)).getAttribute('value')).toEqual('');
+                expect(element(by.css(SELECTORS.INPUT_TO)).getAttribute('value')).toEqual('');
+
+            });
+
+            describe('and middle way points are added', function () {
+
+                it('should provide clear functionality that rests form (incl. way points)', function () {
+
+                    element.all(by.css(SELECTORS.INPUT_FROM)).sendKeys(fromPosition);
+                    element.all(by.css(SELECTORS.INPUT_TO)).sendKeys(toPosition);
+
+                    element.all(by.css(SELECTORS.BTN_ADD_WAY_POINT)).first().click();
+                    element.all(by.css(SELECTORS.INPUT_WAY_POINT + 0)).sendKeys(wayPoints[0]);
+
+                    expect(element(by.css(SELECTORS.INPUT_FROM)).getAttribute('value')).toEqual(fromPosition);
+                    expect(element(by.css(SELECTORS.INPUT_TO)).getAttribute('value')).toEqual(toPosition);
+                    expect(element(by.css(SELECTORS.INPUT_WAY_POINT + 0)).getAttribute('value')).toEqual(wayPoints[0]);
+
+                    element.all(by.css(SELECTORS.BTN_CLEAR)).first().click();
+
+                    expect(element(by.css(SELECTORS.INPUT_FROM)).getAttribute('value')).toEqual('');
+                    expect(element(by.css(SELECTORS.INPUT_TO)).getAttribute('value')).toEqual('');
+
+                    var wayPoint0 = element(by.css(SELECTORS.INPUT_WAY_POINT + 0));
+                    
+                    expect(wayPoint0.isPresent()).toBeFalsy();
+
+                });
+
+            });
+
+        });
+
+        it('should allow to remove add way points', function () {
+
+            var expectedUrl;
+
+            element.all(by.css(SELECTORS.INPUT_FROM)).sendKeys(fromPosition);
+
+            element.all(by.css(SELECTORS.BTN_ADD_WAY_POINT)).first().click();
+            element.all(by.css(SELECTORS.INPUT_WAY_POINT + 0)).sendKeys(wayPoints[0]);
+
+            element.all(by.css(SELECTORS.BTN_ADD_WAY_POINT)).first().click();
+            element.all(by.css(SELECTORS.INPUT_WAY_POINT + 1)).sendKeys(wayPoints[1]);
+
+            element.all(by.css(SELECTORS.INPUT_TO)).sendKeys(toPosition);
+
+            element.all(by.css(SELECTORS.BTN_REMOVE_WAY_POINT)).last().click();
+
+            browser.getCurrentUrl().then(function(url) {
+
+                expectedUrl = getHost() + "/?w0=" + fromPosition + "&w1=" + wayPoints[0] + "&w2=" + toPosition;
+
+                expect(url).toEqual(expectedUrl);
+            });
+
+            element.all(by.css(SELECTORS.BTN_REMOVE_WAY_POINT)).last().click();
+
+            browser.getCurrentUrl().then(function(url) {
+
+                expectedUrl = getHost() + "/?w0=" + fromPosition + "&w1=" + toPosition;
+
+                expect(url).toEqual(expectedUrl);
+            });
+
+        });
+
     });
 });
 
